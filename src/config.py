dc7613066afc55cbc6599373ff6a03e3e1b9681f
IvanDamNation <ivan.suvalkin@gmail.com>
2025-04-17 14:26:04 +0300

    Minor fixes

--- src/config.py
+++ src/config.py
@@ -5,11 +5,6 @@ class AppDomainSettings(BaseSettings):
     APP_HOST: str
     APP_PORT: int
 
-    # model_config = SettingsConfigDict(
-    #     env_file=".env",
-    #     extra="ignore",
-    # )
-
     @property
     def APP_URL(self) -> str:
         return f"http://{self.APP_HOST}:{self.APP_PORT}"
@@ -21,26 +16,20 @@ class RedisSettings(BaseSettings):
     REDIS_PORT: str
     EVENT_TTL: int
 
-    # model_config = SettingsConfigDict(
-    #     env_file=".env",
-    #     extra="ignore",
-    # )
-
 
 class CelerySettings(BaseSettings):
     REDIS_DDUP_HOST: str
     REDIS_QUEUE_HOST: str
     REDIS_PORT: str
 
-    # model_config = SettingsConfigDict(env_file=".env", extra="ignore")
     @property
     def CELERY_BROKER(self) -> str:
         return f"redis://{self.REDIS_QUEUE_HOST}:{self.REDIS_PORT}/0"
-    
+
     @property
     def CELERY_BACKEND(self) -> str:
         return f"redis://{self.REDIS_QUEUE_HOST}:{self.REDIS_PORT}/1"
-    
+
     @property
     def CELERY_DDUP_DB(self) -> str:
         return f"redis://{self.REDIS_DDUP_HOST}:{self.REDIS_PORT}/0"
